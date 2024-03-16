c2dc009c5aa4664fd47c8cf475f8151821b18616
Machado <CMACHA38@ford.com>
2024-03-16 14:45:08 -0300

    Criando e configurando componente referente a caixa contendo o produto e sua descrição

---
+++ src/app/shared/components/product-container/product-container.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../core/models/interfaces/interfaces';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-product-container',
+  standalone: true,
+  imports: [ NgIf, NgFor, FormsModule ],
+  templateUrl: './product-container.component.html',
+  styleUrl: './product-container.component.scss'
+})
+export class ProductContainerComponent {
+
+  @Input({required:true}) product!: Product;
+  @Input({required: true}) accountType!: string;
+  @Output() updateProductEvent = new EventEmitter<Product>();
+  @Output() deleteProductEvent = new EventEmitter<Product>();
+  @Output() addToCartEvent = new EventEmitter<Product>();
+  qty: string = '1'
+
+  showUpdateProductScreen() {
+    this.updateProductEvent.emit(this.product)
+  }
+
+  deleteProduct() {
+    this.deleteProductEvent.emit(this.product)
+  }
+
+  addToCart() {
+    this.product.qty = this.qty
+    this.addToCartEvent.emit(this.product)
+  }
+
+  componentsColor() : string{
+    if(this.accountType.includes("user")) {
+      return 'products__added__container__specific__price user'
+    }
+
+    return 'products__added__container__specific__price admin'
+  }
+}
