ad1f98647935f87163b002cf2a900aebbc87a8db
Machado <CMACHA38@ford.com>
2024-03-16 19:55:27 -0300

    Inserindo componentes de container de formulário dos produtos e seção de produtos

---
+++ src/app/components/admin/product-form/product-form.component.ts
@@ -0,0 +1,160 @@
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output, WritableSignal, signal } from '@angular/core';
+import { Product } from '../../../core/models/interfaces/interfaces';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { v4 as uuid } from 'uuid';
+@Component({
+  selector: 'app-product-form',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './product-form.component.html',
+  styleUrl: './product-form.component.scss'
+})
+export class ProductFormComponent {
+  pizzaIngredients: string[] = [
+    "cheese",
+    "tomato sauce",
+    "pepperoni",
+    "mushrooms",
+    "onions",
+    "bell peppers",
+    "olives",
+    "anchovies",
+    "sausage",
+    "basil",
+    "oregano",
+    "pineapple",
+    "ham",
+    "bacon",
+    "arugula",
+    "sun-dried tomatoes",
+    "feta cheese",
+    "jalapeños",
+    "spinach",
+    "artichokes",
+    "garlic",
+    "chicken",
+    "pesto",
+    "prosciutto"
+  ];
+
+  @Input() showCancelButton : boolean = false;
+  @Input() product!: Product;
+  @Input({required: true}) buttonText: string = '';
+  @Output() formAction: EventEmitter<Product> = new EventEmitter();
+  @Output() cancelButton: EventEmitter<void> = new EventEmitter();
+
+  fileName = "";
+  selectedIngredients: WritableSignal<string[]> = signal([]);
+  productForm! : FormGroup;
+  file!: File;
+  imageBase64!: string;
+  constructor(
+    private productFormBuilder: FormBuilder,
+    private cd : ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    if(this.product) {
+      this.selectedIngredients.set(this.product.ingredients)
+    }
+
+    this.productForm = this.productFormBuilder.group({
+      name: ["", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+        Validators.pattern( /(.|\s)*\S(.|\s)*/)
+      ])],
+      price: ["", Validators.compose([
+        Validators.pattern(/^[0-9]+(\.?[0-9]+)?$/),
+        Validators.pattern( /(.|\s)*\S(.|\s)*/),
+        Validators.minLength(1),
+        Validators.maxLength(4)
+      ])],
+      image: [this.fileName]
+    })
+  }
+
+  ngAfterViewInit(): void {
+    if (this.product) {
+      this.productForm.setValue({
+        "name": this.product.name,
+        "price": this.product.value,
+        "image": ""
+      })
+      
+      this.cd.detectChanges()
+    }
+  }
+
+  insertSelectedIngredient(ingredient: string) : void {
+    !this.selectedIngredients().includes(ingredient)? this.selectedIngredients.update(ingredientsArray => {
+      this.selectedIngredients().push(ingredient)
+      return ingredientsArray
+    }) : "";
+  }
+
+  removeSelectedIngredient(ingredient: string) {
+    let ingredientIndex = this.selectedIngredients().indexOf(ingredient);
+    ingredientIndex > -1? this.selectedIngredients.update(ingredientsArray => {
+      this.selectedIngredients().splice(ingredientIndex, 1)
+      return ingredientsArray
+    }) : "";
+  }
+
+  onFileChange(fileInput : any) {
+    this.file = fileInput.target.files[0];
+    this.productForm.controls['image'].setValue(this.file ? this.file.name : '');
+  }
+
+  convertoBase64() {
+    const reader = new FileReader();
+    reader.onload = async (e: any) => {
+      const base64Image = await e.target.result as string;
+       this.imageBase64 = base64Image;
+      this.cd.detectChanges()
+    };
+    reader.readAsDataURL(this.file);
+  }
+
+  formActionFunc() {
+    if(this.productForm.valid && this.selectedIngredients.length >= 2 && this.selectedIngredients.length < 8) {
+      if(!this.product) {
+        this.convertoBase64()
+        if(this.imageBase64) {
+          this.product = {
+            'id': uuid(),
+            'ingredients': this.selectedIngredients(),
+            'image': this.imageBase64,
+            'name': this.productForm.value["name"],
+            'value': this.productForm.value['price']
+          }
+          this.formAction.emit(this.product)
+          this.productForm.reset()
+          this.selectedIngredients.set([])
+        }
+        return
+      }
+      else if(this.productForm.value["image"] !== "") {
+        this.convertoBase64()
+        if(this.imageBase64){
+          this.product.name = this.productForm.value['name']
+          this.product.value = this.productForm.value['price']
+          this.product.ingredients = this.selectedIngredients()
+          this.product.image = this.imageBase64
+          this.formAction.emit(this.product)
+        }
+        return;
+      }
+
+      this.product.name = this.productForm.value['name']
+      this.product.value = this.productForm.value['price']
+      this.product.ingredients = this.selectedIngredients()
+      this.formAction.emit(this.product)
+    }
+  }
+
+  cancelButtonFunc() {
+    this.cancelButton.emit()
+  }
+}
---
+++ src/app/components/admin/product-section/product-section.component.ts
@@ -0,0 +1,62 @@
+import { Component, WritableSignal, signal } from '@angular/core';
+import { ProductContainerComponent } from '../../../shared/components/product-container/product-container.component';
+import { ProductFormComponent } from '../product-form/product-form.component';
+import { Product } from '../../../core/models/interfaces/interfaces';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { ProductsService } from '../../../core/services/products/products.service';
+import { TitleCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-product-section',
+  standalone: true,
+  imports: [ProductContainerComponent, ProductFormComponent, TitleCasePipe],
+  templateUrl: './product-section.component.html',
+  styleUrl: './product-section.component.scss'
+})
+export class ProductSectionComponent {
+
+  subscription : Subscription = new Subscription();
+  productsArray : WritableSignal<Product[]> = signal([])
+
+  constructor(
+    private router: Router,
+    private productService: ProductsService
+  ) {
+
+  }
+
+  ngOnInit() {
+    this.subscription = this.productService.getProducts().subscribe(
+      (value) => this.productsArray.set(value)
+    )
+  }
+
+  insertProduct(product: Product) {
+    this.productsArray.update(productArray => {
+      productArray.push(product)
+      return productArray
+    })
+    this.subscription = this.productService.postProduct(product).subscribe()
+  }
+
+  updateProduct(product: Product) {
+    this.router.navigate([`/admin/home/products/update/`, product.id])
+  }
+
+  deleteProduct(product: Product) {
+    let productIndex = this.productsArray().indexOf(product)
+    this.subscription = this.productService.deleteProduct(product.id).subscribe(
+      {
+        next: () => {
+        this.productsArray.update(productArray => {
+          productArray.splice(productIndex, 1)
+          return productArray
+        })
+      }}
+    )
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+}
